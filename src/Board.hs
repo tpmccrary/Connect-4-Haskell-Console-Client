ec310d18f507bfece0d56e21a4ba6a54755e86a5
tpmccrary <timothypmccrary@gmail.com>
2020-11-27 21:50:37 -0700

    Added mkPlayer and mkOpponent.

--- src/Board.hs
+++ src/Board.hs
@@ -1,3 +1,12 @@
 module Board where
+
+    -- Access player, assigned to 1.
+    mkPlayer :: (Integer, Char)
+    mkPlayer = (1, 'X')
+    -- Access opponent, assign to 2.
+    mkOpponent :: (Integer, Char)
+    mkOpponent = (2, 'O')
+
+    -- Creates a board of m by n.
     mkBoard :: Int -> Int -> [[Int]]
     mkBoard m n = replicate m (replicate n 0 )
