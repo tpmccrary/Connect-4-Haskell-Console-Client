badd754af5f0c3013fb24b859b1dc827b20f43c6
Jesus Maximino Hernandez <jesusmaxhernandez@gmail.com>
2020-11-28 00:06:56 -0700

    Added dropInSlot and replaceSlot method

--- src/Board.hs
+++ src/Board.hs
@@ -10,3 +10,25 @@ module Board where
     -- Creates a board of m by n.
     mkBoard :: Int -> Int -> [[Int]]
     mkBoard m n = replicate m (replicate n 0 )
+
+    --Finds which slot to drop the piece
+    dropInSlot :: [[Int]] -> Int -> Int -> [[Int]]
+    dropInSlot [] _ _ = [[]]
+    dropInSlot bd i p 
+        | not (isSlotOpen bd i) = []
+        | replaceSlot bd p (numRows bd) i
+
+    --Replaces the empty slot with player number
+    replaceSlot :: [[Int]] -> Int -> Int -> Int -> [[Int]]
+    replaceSlot board player row col
+        | row < 0 = board
+        | board!!row!!col == 0 = (take row board ++ [take col (board!!row) ++ [player] ++ drop (col+1) (board!!row)] ++ drop (row+1) board) -- If empty Slot update board
+        | otherwise = replaceSlot board player (row-1) col -- Slot Taken
+    
+    -- returns num of rows - 1
+    numRows :: [[Int]] -> Int
+    numRows bd = length (bd) - 1
+
+    -- returns num of columns
+    numSlot :: [[Int]] -> Int
+    numSlot bd = length (bd!!0)
