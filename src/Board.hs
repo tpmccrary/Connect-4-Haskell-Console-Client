7bff6dd8a0af24178545d1c6ecd1d16eb4480ec1
tpmccrary <timothypmccrary@gmail.com>
2020-11-27 23:08:23 -0700

    Added isSlotOpen and isFull

--- src/Board.hs
+++ src/Board.hs
@@ -10,3 +10,14 @@ module Board where
     -- Creates a board of m by n.
     mkBoard :: Int -> Int -> [[Int]]
     mkBoard m n = replicate m (replicate n 0 )
+
+    -- Checks to see if slot i is open on board bd.
+    isSlotOpen :: [[Int]] -> Int -> Bool
+    isSlotOpen [[]] _ = False
+    isSlotOpen bd i = if bd !! 0 !! i == 0 then True else False
+
+    -- Checks if the board is full.
+    isFull :: [[Int]] -> Int -> Bool
+    isFull bd i = if i >= numSlot bd then True
+    else if isSlotOpen bd i then False
+    else isFull bd (i + 1) 
